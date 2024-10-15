d3047bd59e598afcb06c6f1d492618b32d979380
Vitorcostalv <vitorcosta99900@gamil.com>
2024-10-15 01:19:45 -0300

    Aula 1, revisando alguns conceitos que com o tempo acabei esquecendo por falta de pratica com a  linguagem

---
+++ Aulas/Aula1.py
@@ -0,0 +1,12 @@
+# Aula 1: Olá, Mundo e variáveis
+print("Olá, Mundo!")  # Exibe a mensagem "Olá, Mundo!" na tela
+
+# Trabalhando com variáveis
+nome = "Vitor"
+idade = 20
+altura = 1.75
+
+# Exibindo os valores das variáveis
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
